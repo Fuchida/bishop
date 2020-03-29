2922e5960f5b4cd5fe8295f5802dcee520032b05
Abdul Salim <abdlsalim@gmail.com>
2020-03-28 20:24:17 -0500

    Return empty dict when key is not found and minor name changes

--- app/service.py
+++ app/service.py
@@ -13,7 +13,6 @@ class MetaStore:
 
     def __init__(self):
         self.env = EnvironmentSettings()
-        breakpoint()
         self.store = bucketstore.get(self.env.s3_bucket_name, create=True)
 
     def get(self, collection: str, key: str) -> dict:
@@ -23,13 +22,18 @@ class MetaStore:
             Raises:
                 NoSuchKey when key does not exists
         """
-        return json.loads(self.store.get(f'{collection}/{key}'))
+        if self.key_exists(collection, key):
+            data = json.loads(self.store.get(f'{collection}/{key}'))
+        else:
+            data = {}
 
-    def put(self, collection: str, key: str, payload: dict) -> dict:
+        return data
+
+    def put(self, collection: str, key: str, data: dict) -> dict:
         """
             Provided a key and value store data in the datastore
         """
-        return self.store.set(f'{collection}/{key}', json.dumps(payload, default=str))
+        return self.store.set(f'{collection}/{key}', json.dumps(data, default=str))
 
     def key_exists(self, collection: str, key: str) -> bool:
         """
